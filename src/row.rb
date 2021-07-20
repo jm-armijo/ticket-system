1454b82facc5fb012da472362e54c6a1afce355f
Jose Miguel Armijo <jm.armijo.f@gmail.com>
2021-07-20 23:58:59 +1000

    Provide access to row columns Ensure that raw data (hash) use symbols as keys

--- src/row.rb
+++ src/row.rb
@@ -1,5 +1,18 @@
 class Row
     def initialize(data)
         @data = data
+        @data&.transform_keys!(&:to_sym)
+    end
+
+private
+
+    def method_missing(name)
+        raise "Invalid column name #{name}" if !respond_to_missing?(name)
+
+        return @data[name.to_sym]
+    end
+
+    def respond_to_missing?(name)
+        return @data.key?(name.to_sym)
     end
 end
--- src/row_spec.rb
+++ src/row_spec.rb
@@ -4,6 +4,8 @@ require_relative './row'
 describe Row do
     it 'should save data on initialization' do
         mock_data = double
+        allow(mock_data).to receive(:transform_keys!)
+
         row = Row.new(mock_data)
         expect(row.instance_variable_get(:@data)).to equal(mock_data)
     end
@@ -11,5 +13,31 @@ describe Row do
     it 'should save data on initialization even if it is nil' do
         row = Row.new(nil)
         expect(row.instance_variable_get(:@data)).to be_nil
+    end
+
+    it 'should save keys as symbols when passed as symbols' do
+        data = { key1: 'value1', key2: 'value2' }
+        row = Row.new(data)
+        expect(row.instance_variable_get(:@data).keys.all? { |k| k.is_a?(Symbol) }).to be(true)
+    end
+
+    it 'should save keys as symbols when passed as strings' do
+        data = { key1: 'value1', key2: 'value2' }
+        row = Row.new(data)
+        expect(row.instance_variable_get(:@data).keys.all? { |k| k.is_a?(Symbol) }).to be(true)
+    end
+
+    it 'should give access key given as symbol' do
+        data = { key1: 'value1', key2: 'value2' }
+        row = Row.new(data)
+
+        expect(row.key1).to eq('value1')
+    end
+
+    it 'should give access key given as string' do
+        data = { 'key1' => 'value1', 'key2' => 'value2' }
+        row = Row.new(data)
+
+        expect(row.key1).to eq('value1')
     end
 end
